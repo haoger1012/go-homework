28cbf9c2d50fa7f35e81f9e7351948136c1408d3
haoger1012 <haoger1012@gmail.com>
2020-11-28 13:05:57 +0800

    implement CRUD

--- hw/go.mod
+++ hw/go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
 	github.com/swaggo/gin-swagger v1.3.0
 	github.com/swaggo/swag v1.5.1
+	golang.org/x/net v0.0.0-20190611141213-3f473d35a33a
 )
--- hw/main.go
+++ hw/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"golang.org/x/net/context"
 	"net/http"
+	"strconv"
+
+	"golang.org/x/net/context"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +13,7 @@ func main() {
 	context.Background()
 	router := gin.Default()
 	http.HandleFunc("ss", func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w,r)
+		http.NotFound(w, r)
 	})
 	router.GET("/role", Get)
 
@@ -33,12 +35,30 @@ func Get(c *gin.Context) {
 
 // 取得單一筆資料
 func GetOne(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
 
+	for i := 0; i < len(Data); i++ {
+		if Data[i].ID == uint(id) {
+			c.JSON(http.StatusOK, Data[i])
+			return
+		}
+	}
+	c.Status(http.StatusNotFound)
 }
 
 // 新增資料
 func Post(c *gin.Context) {
-
+	var r Role
+	if err := c.ShouldBind(&r); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+	Data = append(Data, r)
+	c.JSON(http.StatusOK, r)
 }
 
 type RoleVM struct {
@@ -49,10 +69,42 @@ type RoleVM struct {
 
 // 更新資料, 更新角色名稱與介紹
 func Put(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
 
+	var r Role
+	if err := c.ShouldBind(&r); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
+
+	for i := 0; i < len(Data); i++ {
+		if Data[i].ID == uint(id) {
+			Data[i].Name = r.Name
+			Data[i].Summary = r.Summary
+			c.JSON(http.StatusOK, Data[i])
+			return
+		}
+	}
 }
 
 // 刪除資料
 func Delete(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		return
+	}
 
+	for i, role := range Data {
+		if role.ID == uint(id) {
+			Data = append(Data[0:i], Data[i+1:]...)
+			break
+		}
+	}
+
+	c.Status(http.StatusNoContent)
 }
